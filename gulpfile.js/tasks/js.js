f28ffdafc39a4fd2ff66056e0be3bd0713b4ba13
Cotes Chung <11371340+cotes2020@users.noreply.github.com>
2022-01-26 22:06:44 +0800

    Improve local datetime display
    - Protect author's location privacy
    - Reduce locale configuration parameters

--- gulpfile.js/tasks/js.js
+++ gulpfile.js/tasks/js.js
@@ -72,12 +72,21 @@ const pageJs = () => {
   );
 };
 
+const miscJs = () => {
+  return concatJs([
+      `${JS_SRC}/commons/*.js`,
+      `${JS_SRC}/utils/locale-datetime.js`
+    ], 'misc'
+  );
+};
+
 // GA pageviews report
 const pvreportJs = () => {
   return concatJs(`${JS_SRC}/utils/pageviews.js`, 'pvreport');
 };
 
-const buildJs = parallel(commonsJs, homeJs, postJs, categoriesJs, pageJs, pvreportJs);
+const buildJs = parallel(
+  commonsJs, homeJs, postJs, categoriesJs, pageJs, miscJs, pvreportJs);
 
 exports.build = series(buildJs, minifyJs);
 
@@ -86,8 +95,7 @@ exports.liveRebuild = () => {
 
   watch([
       `${ JS_SRC }/commons/*.js`,
-      `${ JS_SRC }/utils/*.js`,
-      `${ JS_SRC }/lib/*.js`
+      `${ JS_SRC }/utils/*.js`
     ],
     buildJs
   );
